a93508d12bd1ac12d6012e4bc9211bd8abcb3ab7
rtazaki <rtazaki@gmail.com>
2018-07-14 13:38:31 +0900

    ChoiceBoxお試し開始

--- src/main/kotlin/sample/com/MainController.kt
+++ src/main/kotlin/sample/com/MainController.kt
@@ -2,11 +2,11 @@ package sample.com
 
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
+import javafx.scene.control.ChoiceBox
 import javafx.scene.control.RadioButton
 import javafx.scene.control.TextField
 import javafx.scene.control.ToggleGroup
 import javafx.scene.paint.Color
-import javafx.scene.paint.Paint
 import javafx.scene.shape.Rectangle
 import java.net.URL
 import java.util.*
@@ -27,11 +27,14 @@ class MainController : Initializable {
     @FXML
     lateinit var rgbColor2: RadioButton
 
+    @FXML
+    lateinit var rgbChoice: ChoiceBox<String>
+
     override fun initialize(location: URL?, resources: ResourceBundle?) {
         // TextField -> RadioButton
         // idをindexのように使うためのMap
         val map1 = mapOf(0 to rgbColor0, 1 to rgbColor1, 2 to rgbColor2)
-        rgbIndexField.textProperty().addListener({ _, _, newValue ->
+        rgbIndexField.textProperty().addListener { _, _, newValue ->
             try {
                 if (newValue.toInt() < 3) {
                     println(newValue)
@@ -40,11 +43,11 @@ class MainController : Initializable {
             } catch (exception: NumberFormatException) {
                 // 数字以外の入力は無視
             }
-        })
+        }
         // RadioButton -> TextField
         // Red→Color.REDにする
         val map2 = mapOf("Red" to Color.RED, "Blue" to Color.BLUE, "Green" to Color.GREEN)
-        rgbColorGroup.selectedToggleProperty().addListener({ _, _, newValue ->
+        rgbColorGroup.selectedToggleProperty().addListener { _, _, newValue ->
             println(newValue)
             // 選択したラジオボタンのidを拾うための、間違った方向の努力
             val id = Regex("""id=[^,]+""")
@@ -53,6 +56,9 @@ class MainController : Initializable {
             val color = Regex("""'[^']+""")
             val colorstr = color.find(newValue.toString())?.value?.replace("'", "")
             rgbResultRectangle.fill = map2[colorstr]
-        })
+        }
+        // 選択画面 -> TextField
+        rgbChoice.items.addAll("赤", "緑", "青")
+        rgbChoice.pr
     }
 }
